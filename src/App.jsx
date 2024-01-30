cbadb7603237bfc1cb11909cbf62425322a8ecf8
Priyam More <morepriyam99@gmail.com>
2024-01-30 22:50:37 +0530

    Positining

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import "./App.css";
 import Background from "./Components/Bakcground/Background";
 import { useState } from "react";
 function App() {
-  const [bgColor, setBgColor] = useState("#2F3437");
+  const [bgColor, setBgColor] = useState(
+    "linear-gradient(45deg, #FFA500, #FFD700, #FFA500, #FFD700)"
+  );
   return (
-    <div className="App" style={{ backgroundColor: bgColor }}>
+    <div className="App" style={{ backgroundImage: bgColor }}>
       <Profile />
       <Background setBgColor={setBgColor} />
     </div>
--- src/Components/Bakcground/Background.jsx
+++ src/Components/Bakcground/Background.jsx
@@ -5,25 +5,62 @@ function Background({ setBgColor }) {
   return (
     <div className="colors">
       <div className="buttons">
-        <button className="red" onClick={() => setBgColor("red")}>
+        <button
+          className="red"
+          onClick={() =>
+            setBgColor("linear-gradient(to right, #FF0000, #FF6666)")
+          }
+        >
           Red
         </button>
-        <button className="yellow" onClick={() => setBgColor("yellow")}>
+        <button
+          className="yellow"
+          onClick={() =>
+            setBgColor("linear-gradient(to right, #FFFF00, #FFFF99)")
+          }
+        >
           Yellow
         </button>
-        <button className="black" onClick={() => setBgColor("black")}>
+        <button
+          className="black"
+          onClick={() =>
+            setBgColor("linear-gradient(to right, #000000, #333333)")
+          }
+        >
           Black
         </button>
-        <button className="purple" onClick={() => setBgColor("purple")}>
+        <button
+          className="purple"
+          onClick={() =>
+            setBgColor("linear-gradient(to right, #800080, #B300B3)")
+          }
+        >
           Purple
         </button>
-        <button className="green" onClick={() => setBgColor("green")}>
+        <button
+          className="green"
+          onClick={() =>
+            setBgColor("linear-gradient(to right, #008000, #66FF66)")
+          }
+        >
           Green
         </button>
-        <button className="blue" onClick={() => setBgColor("blue")}>
+        <button
+          className="blue"
+          onClick={() =>
+            setBgColor("linear-gradient(to right, #0000FF, #3399FF)")
+          }
+        >
           Blue
         </button>
-        <button className="default" onClick={() => setBgColor("#2F3437")}>
+        <button
+          className="default"
+          onClick={() =>
+            setBgColor(
+              "linear-gradient(45deg, #FFA500, #FFD700, #FFA500, #FFD700)"
+            )
+          }
+        >
           Default
         </button>
       </div>
